dd026ddaaa607cba9013431c7a295991afe0d85e
programodev <freelancervenkatb@gmail.com>
2025-12-11 20:07:15 +0530

    Delete assets/js/footer-inject.js

--- assets/js/footer-inject.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fetch("/_includes/footer.html")
-    .then(response => response.text())
-    .then(html => {
-      const footer = document.createElement("div");
-      footer.innerHTML = html;
-      document.body.appendChild(footer);
-    });
-});
